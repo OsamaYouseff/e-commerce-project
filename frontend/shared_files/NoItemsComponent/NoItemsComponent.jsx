f5f17a704bf4472b248b4cfeb694bb3b3d826c05
OsamaYouseff <oy6305616@gmail.com>
2024-09-11 16:04:22 +0300

    fix small bug in component's theme

--- frontend/shared_files/NoItemsComponent/NoItemsComponent.jsx
+++ frontend/shared_files/NoItemsComponent/NoItemsComponent.jsx
@@ -1,16 +1,20 @@
 /* eslint-disable react/prop-types */
 
+import { useTheme } from "@emotion/react";
+import { ColorModeContext } from "../Theme/theme";
+
 import { Box } from "@mui/material";
 
 const NoItemsComponent = ({ message, minHeight, fontSize }) => {
+    const theme = useTheme(ColorModeContext);
 
     return (
         <Box
             className="flex-center"
             sx={{
                 p: 2,
-                bgcolor: "sectionBgColor.main",
-                color: "text.primary",
+                bgcolor: theme.palette.sectionBgColor.main,
+                color: theme.palette.text.main,
                 textAlign: "center",
                 borderRadius: "10px",
                 fontWeight: "bolder",
